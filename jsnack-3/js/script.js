50a9ccbd09271f5c8cfa653c29ceee52a537db71
bodascaluca <bodasca.luca@gmail.com>
2022-04-20 15:21:38 +0200

    jsnack-3 problem with the number

---
+++ jsnack-3/js/script.js
@@ -0,0 +1,15 @@
+// jsnack3. Stampa il cubo dei primi N numeri, dove N è un numero indicato dall'utente.
+
+const numeroDato = parseInt(prompt(`Dimmi un numeoro`));
+console.log(numeroDato);
+
+// const numeroCubo = (Math.pow(numeroDato, 3)) ;
+// console.log(numeroCubo);
+
+for (let i = 0; i <= numeroDAto; i++){
+    const numeroCubo = (Math.pow(numeroDato, 3)) ;
+    console.log(numeroCubo);
+
+}
+
+
