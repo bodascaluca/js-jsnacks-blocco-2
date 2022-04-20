5269d11e325589b3e3faa11234412d147b129cb3
bodascaluca <bodasca.luca@gmail.com>
2022-04-20 16:52:31 +0200

    start da jsnack-1

--- jsnack-1/js/script.js
+++ jsnack-1/js/script.js
@@ -1,1 +1,14 @@
-alert("ciao");
+// jsnack1. Il software deve chiedere per 5 volte all'utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+// Esegui questo programma in due versioni, con il for e con il while.
+
+for (let i = 0; i < 5; i++){
+    const numeroDato = parseInt(prompt(`dimmi un numero`)); 
+    console.log(numeroDato);
+}
+
+let sum = 0;
+for(let i = 0; i < numeroDato; i++){
+    sum += [i];
+    console.log(sum);
+}
