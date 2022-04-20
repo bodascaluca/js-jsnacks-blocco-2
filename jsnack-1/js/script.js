57166aab1998a5e335d96fa398ab771041047d27
bodascaluca <bodasca.luca@gmail.com>
2022-04-20 17:34:36 +0200

    add jsnake-1

--- jsnack-1/js/script.js
+++ jsnack-1/js/script.js
@@ -2,13 +2,45 @@
 // Il programma stampa la somma di tutti i numeri inseriti.
 // Esegui questo programma in due versioni, con il for e con il while.
 
-for (let i = 0; i < 5; i++){
-    const numeroDato = parseInt(prompt(`dimmi un numero`)); 
-    console.log(numeroDato);
-}
 
+// FOR
+// let sum = 0;
+// for (let i = 0; i < 5; i++){
+//     const numeroDato = parseInt(prompt(`dimmi un numero`)); 
+//     sum += numeroDato;
+// }
+// console.log(sum);
+
+
+
+
+// WHILE
+// let richiesta = 0;
+// let sum = 0;
+
+// while(richiesta < 5){
+//     //  console.log("interazione", richiesta);
+//     const numeroDato = parseInt(prompt(`dimmi un numero`)); 
+//     sum += numeroDato;
+//     richiesta++;
+// }
+// console.log(sum);
+
+
+
+
+// !isNaN    PER VERIFICARE SE E' UN NUMERO O MENO
+let richiesta = 0;
 let sum = 0;
-for(let i = 0; i < numeroDato; i++){
-    sum += [i];
-    console.log(sum);
+
+while(richiesta < 5){
+    const numeroDato = parseInt(prompt(`dimmi un numero`)); 
+    if (!isNaN(numeroDato)){
+        sum += numeroDato;
+        richiesta++;
+    } else {
+        alert(`Non è un numero`);
+    }
+    
 }
+console.log(sum);
